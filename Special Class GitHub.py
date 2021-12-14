386bfb81b0a38e2e032c7722404f2a1ba492f87d
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-12-14 19:27:15 +0530

    Add files via upload

---
+++ Special Class GitHub.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+Marks = [60, 65, 70, 50, 75, 80, 95, 85, 100, 90, 80, 40, 50, 75, 75, 95, 70, 99, 50, 75]
+
+plt.hist(Marks)
+
+plt.xlabel('Marks')
+plt.ylabel('Count')
+
+plt.title("Frequency count of marks obtained")
+
+plt.show()
